138747d613a3f67a900296d54f6e7a41729f5717
masud4657 <masudpfrnt@gmail.com>
2024-05-22 22:03:48 +0600

    added the post crud operation to add cart item in the mongoDb.

--- index.js
+++ index.js
@@ -28,9 +28,22 @@ async function run() {
     await client.connect();
 
   const menuCollection = client.db("bistroDb").collection("menu")
+  const reviewCollection = client.db("bistroDb").collection("reviews")
+  const cartCollection = client.db("bistroDb").collection("carts")
 
   app.get('/menu',async(req,res)=>{
     const result = await menuCollection.find().toArray()
+    res.send(result)
+  })
+
+  app.get('/review',async(req,res)=>{
+    const result = await reviewCollection.find().toArray()
+    res.send(result)
+  })
+
+  app.post('/cart',async(req,res)=>{
+    const cartItem = req.body
+    const result = await cartCollection.insertOne(cartItem)
     res.send(result)
   })
 
