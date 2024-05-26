0ac503c2f4baa79c6fe200831e89b5d5a25e9d81
masud4657 <masudpfrnt@gmail.com>
2024-05-26 13:05:28 +0600

    added the different crud operations.

--- index.js
+++ index.js
@@ -129,6 +129,37 @@ async function run() {
     const result = await menuCollection.insertOne(item)
     res.send(result)
   })
+
+  app.delete('/menu/:id', verifyToken,verifyAdmin,async(req,res)=>{
+    const id = req.params.id
+    const query={_id:id}
+    const result = await menuCollection.deleteOne(query)
+    res.send(result)
+  })
+  app.get('/menu/:id',async(req,res)=>{
+    const id = req.params.id
+    const query = {_id:id}
+    const result =await menuCollection.findOne(query)
+    res.send(result)
+  })
+
+  app.patch('/menu/:id',async(req,res)=>{
+    const item = req.body
+    const id = req.params.id
+    const filter ={_id:id}
+    const updatedDoc={
+      $set:{
+        name:item.name,
+        category:item.category,
+        price:item.price,
+        recipe:item.recipe,
+        image:item.image,
+        
+      }
+    }
+    const result =await menuCollection.updateOne(filter,updatedDoc)
+    res.send(result)
+  })
 // client review related api
   app.get('/review',async(req,res)=>{
     
